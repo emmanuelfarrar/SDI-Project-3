64796173b91cf080c1b26a404b67f0b8afbabc05
emmanuelfarrar <emmanuel.farrar@fullsail.edu>
2014-06-19 20:38:18 +0430

    Change 2
    Added the procedure. This is just a test to test outputs. The procedure
    needs to be tuned to properly be used in this project.

--- farrar_emmanuel_assignment3.js
+++ farrar_emmanuel_assignment3.js
@@ -5,9 +5,18 @@ SDI Project 3
 */
 
 //GLOBAL VARIABLES
+var jobName = ["Paladin", "Warrior", "Dragoon", "Monk", "Ninja", "Summoner", "Black Mage", "White Mage", "Scholar"];
 
 
 //FUNCTIONS
 
+//procedure to just show the array
+var myProcedure = function (jobArray) 
+{
+	console.log("The array is: " + jobArray);  //list the elements of the arrray
+	//console.log(jobArray);
+};
 
 //MAIN CODE
+
+//myProcedure(jobName);
---
+++ farrar_emmanuel_json.js
@@ -0,0 +1,27 @@
+//alert("JavaScript works!");
+/* Emmanuel Farrar
+Date: 19JUN2014
+SDI Project 3
+*/
+
+//JSON Data
+var json = 	{
+		"players": [
+			{
+				"name": "Mannie",
+				"inGameName": "Xeon"
+			},
+			{
+				"name": "Dave",
+				"inGameName": "Sheldor"
+			},
+			{
+				"name": "Rachel",
+				"inGameName": "Terra"
+			},
+			{
+				"name": "Otis",
+				"inGameName": "Trogdar"
+			}
+		]
+	};
