bbc81d25d066274e455f9457ea62645625da5490
emmanuelfarrar <emmanuel.farrar@fullsail.edu>
2014-06-19 20:57:51 +0430

    Change 3
    Added string function and tested basic functionality with if statements.

--- farrar_emmanuel_assignment3.js
+++ farrar_emmanuel_assignment3.js
@@ -13,10 +13,30 @@ var jobName = ["Paladin", "Warrior", "Dragoon", "Monk", "Ninja", "Summoner", "Bl
 //procedure to just show the array
 var myProcedure = function (jobArray) 
 {
-	console.log("The array is: " + jobArray);  //list the elements of the arrray
+	console.log("The array is: " + jobArray);  //list the elements of the array
 	//console.log(jobArray);
 };
+
+//string function to identify weapon type and if coil turn has been done before.
+var myStrFunction = function (weapon, coil)
+{
+	//Need to declare local variable, return & use it in MAIN CODE
+	
+	if (weapon === "Novus")
+	{
+		console.log("You have a shiny weapon");
+	}
+	if (weapon === "Animus")
+	{
+		console.log("You still have some work to do.");
+	}
+	
+};
+
+
 
 //MAIN CODE
 
 //myProcedure(jobName);
+myStrFunction("Novus", "Yes");
+myStrFunction("Animus", "Yes");
