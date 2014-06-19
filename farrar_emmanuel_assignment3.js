5cad13199e59c487649d8e7fda1549de80b170c5
emmanuelfarrar <emmanuel.farrar@fullsail.edu>
2014-06-19 21:16:05 +0430

    Change 4
    Added element to to Array but will remove and add via method; added
    basic boolean function and tested it and its return.

--- farrar_emmanuel_assignment3.js
+++ farrar_emmanuel_assignment3.js
@@ -5,7 +5,7 @@ SDI Project 3
 */
 
 //GLOBAL VARIABLES
-var jobName = ["Paladin", "Warrior", "Dragoon", "Monk", "Ninja", "Summoner", "Black Mage", "White Mage", "Scholar"];
+var jobName = ["Paladin", "Warrior", "Dragoon", "Monk", "Bard", "Ninja", "Summoner", "Black Mage", "White Mage", "Scholar"];
 
 
 //FUNCTIONS
@@ -33,10 +33,24 @@ var myStrFunction = function (weapon, coil)
 	
 };
 
+//boolean function
+var myBooFuncation = function (youbard)
+{
+//make this the nested conditional function
+if (youbard === true)
+{
+	myConfirm = confirm("Since you are a bard are you okay with kiting the adds?");
+}
+	return myConfirm;
+};
 
+//number function
+var myNumFunction
 
 //MAIN CODE
 
-//myProcedure(jobName);
-myStrFunction("Novus", "Yes");
-myStrFunction("Animus", "Yes");
+//myProcedure(jobName);   // to test procedure
+myStrFunction("Novus", "Yes");    // to test string function
+myStrFunction("Animus", "Yes");   // to test string function
+myBooFuncation(true);             // to test boolean function
+console.log ("The bard is okay with kitting: " + myConfirm);  //to test return for boolean function
